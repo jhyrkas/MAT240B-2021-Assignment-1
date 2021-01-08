2432837d4f7570c58d9a1ba01a16ef2cd5dda40c
Jeremy Hyrkas <hyrkas@Jeremys-iMac.local>
2021-01-08 15:14:41 -0800

    square-sweep

--- square-sweep.cpp
+++ square-sweep.cpp
@@ -1,1 +1,28 @@
+#include "everything.h"
 
+int main(int argc, char* argv[]) {
+  float phase = 0;
+
+  double nyquist = SAMPLE_RATE / 2.0;
+  double cutoff = 0.8 * nyquist;
+
+  for (float note = 127; note > 0; note -= 0.001) {
+    float frequency = mtof(note);
+
+    // now we have to do the cutoff inside of the loop
+    double sample = 0;
+    float tmp = frequency;
+    int j = 1;
+    while (tmp < cutoff) {
+        sample += (sin(j * phase) / j);
+        tmp += (frequency*2);
+        j += 2;
+    }
+    sample *= (4.0 / pi);
+    mono(sample);
+
+    phase += 2 * pi * frequency / SAMPLE_RATE;
+    if (phase > 2 * pi)  //
+      phase -= 2 * pi;
+  }
+}
